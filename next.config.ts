a0e1096ca1f6d94037c5afb83765d0445eed046b
Ling Cong <lingcong@aktez.com.my>
2025-04-01 10:20:50 +0800

    Revert "init"
    This reverts commit 7bea275a48e788671f01f70d208dfaad3da3b41b.

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
-      },
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
-  reactStrictMode: false,  
+  /* config options here */
 };
 
 export default nextConfig;
--- src/model/VehicleModel.ts
+++
@@ -1,8 +0,0 @@
-export type VehicleModel = {
-    id: number;
-    name: string;
-    model: string;
-    platNo: string;
-    desc: string;
-    price: number;
-}
